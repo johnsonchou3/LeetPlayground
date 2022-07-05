7c393731e5451d99c91f3e85ecac5db727b68e94
Johnson Chou <johnsonchou3@gmail.com>
2022-07-05 23:03:30 +0800

    Longest Consecutive Sequence

---
+++ Playground/LongestConsecutiveSequence.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playground
+{
+    public class LongestConsecutiveSequence
+    {
+        public int LongestConsecutive(int[] nums)
+        {
+            var set = new HashSet<int>();
+            var longest = 0;
+            foreach (var n in nums)
+            {
+                set.Add(n);
+            }
+
+            foreach (var n in nums)
+            {
+                if (!set.Contains(n - 1))
+                {
+                    var curLength= 1;
+                    var currentNum = n;
+                    while (set.Contains(curLength + 1))
+                    {
+                        curLength++;
+                        currentNum++;
+                    }
+                    longest = Math.Max(longest, curLength);
+                }
+            }
+            return longest;
+        }
+
+    }
+}

9675848c53c812da002ee7f7c64121ee272c1b0f
Johnson Chou <johnsonchou3@gmail.com>
2022-07-12 00:11:24 +0800

    Longest Increasing Subsequence

---
+++ Playground/LongestIncreasingSubsequence.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playground
+{
+    public class LongestIncreasingSubsequence
+    {
+        public int LengthOfLIS(int[] nums)
+        {
+            var dp = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                dp[i] = 1;
+                for (int j = 0; j < i; j++)
+                {
+                    if (nums[i] > nums[j])
+                    {
+                        dp[i] = Math.Max(dp[i], dp[j] + 1);
+                    }
+                }
+            }
+            return dp.Max();
+        }
+    }
+}
--- Playground/Program.cs
+++ Playground/Program.cs
@@ -1,7 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using Playground;
 
-//var q = new NumberofLongestIncreasingSubsequence();
-var payload = new []
-{1, 3, 5, 4, 7};
-//Console.WriteLine( q.FindNumberOfLIS(payload));
+var q = new SortedList<int, int>();
+q.Add(3, 5);
+q.Add(1, 300000);
+q.Add(22, 3);
+// 用前面排, 前面是Key
+Console.WriteLine(q.ElementAt(0));

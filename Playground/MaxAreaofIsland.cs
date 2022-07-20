5b607563061ba69d95a6a7bdaaf352962cfad26d
Johnson Chou <johnsonchou3@gmail.com>
2022-07-20 22:41:44 +0800

    Pascal

---
+++ Playground/MaxAreaofIsland.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playground
+{
+    internal class MaxAreaofIslandLeet
+    {
+        public int MaxAreaOfIsland(int[][] grid)
+        {
+            var maxArea = 0;    
+            for (int i = 0; i < grid.Length; i++)
+            {
+                for (int j = 0; j < grid[0].Length; j++)
+                {
+                    if (grid[i][j] == 1)
+                    {
+                        grid[i][j] = 0;
+                        maxArea = Math.Max(maxArea, DFS(i, j));
+                    }
+                }
+            }
+            return maxArea;
+
+            int DFS(int i, int j)
+            {
+                int totalArea = 1;
+                if (j!= 0 &&  grid[i][j - 1] == 1)
+                {
+                    grid[i][j - 1] = 0;
+                    totalArea += DFS(i, j - 1);
+                }
+                if (j!= grid[0].Length - 1 && grid[i][j + 1] == 1)
+                {
+                    grid[i][j + 1] = 0;
+                    totalArea += DFS(i, j + 1);
+                }
+                if (i!= 0 && grid[i - 1][j] == 1)
+                {
+                    grid[i - 1][j] = 0;
+                    totalArea += DFS(i - 1, j);
+                }
+                if (i != grid.Length - 1 && grid[i + 1][j] == 1)
+                {
+                    grid[i + 1][j] = 0;
+                    totalArea += DFS(i + 1, j);
+                }
+                return totalArea;
+            }
+        }
+    }
+}
---
+++ Playground/PascalTriangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Playground
+{
+    public class PascalTriangle
+    {
+        public IList<IList<int>> Generate(int numRows)
+        {
+            int row = numRows;
+            var dp = new int[row][];
+
+            for (int i = 0; i < dp.Length; i++)
+            {
+                dp[i] = new int[i + 1];
+                // set the first and last cell value to 1
+                dp[i][0] = dp[i][i] = 1;
+
+                // will start setting from 2nd cell, index = 1
+                for (int j = 1; j < i; j++)
+                {
+                    // (current cell from previous row + previous cell from previous row)
+                    dp[i][j] = dp[i - 1][j] + dp[i - 1][j - 1];
+                }
+            }
+
+            return dp;
+        }
+    }
+}

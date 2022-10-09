c9dd39d46033cc73b832770b3f5f794a1326231e
Johnson Chou <johnsonchou3@gmail.com>
2022-10-10 01:44:51 +0800

    Two Sum IV - Input is a BST

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -3,5 +3,7 @@ using Playground;
 using System.Net.Http.Json;
 using System.Net.Security;
 
-var x = new Binary_Search();
-x.Search(new []{-1,0,3,5,9,12}, 2);
+var s = new HashSet<int>();
+s.Add(1);
+s.Add(1);
+s.Add(1);
---
+++ Playground/Two Sum IV - Input is a BST.cs
@@ -0,0 +1,28 @@
+﻿namespace Playground;
+
+public class Two_Sum_IV___Input_is_a_BST
+{
+    public bool FindTarget(TreeNode root, int k)
+    {
+        var set = new HashSet<int>();
+        var hasPair = false;
+        DFS(root);
+        return hasPair;
+        void DFS(TreeNode node)
+        {
+            if (node == null)
+            {
+                return;
+            }
+
+            if (set.Contains(node.val))
+            {
+                hasPair = true;
+                return;
+            }
+            set.Add(k - node.val);
+            DFS(node.left);
+            DFS(node.right);
+        }
+    }
+}
